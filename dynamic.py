2c3e55ac5fb3324c70863f843e52ae469d84d3b0
Kirk Abbott <kirkabbott1@gmail.com>
2016-09-28 15:50:55 -0400

    updates dictionary practice

--- dynamic.py
+++ dynamic.py
@@ -2,35 +2,49 @@
 #
 # Download the programmers_blues.txt file. Write a program to read in the text of that file and count the number of occurrences of each word in that file. You will print one line for each word encountered, and output the number of times that word appearred in the file. The output should look something like:
 #
-# $ python word_count.py
-# code: 1
-# help: 1
-# just: 3
-# don't: 1
-# hand,: 1
-# better,: 1
-# copped,: 1
-# won't: 1
-# keyboard: 1
-# ...
-# programmer's: 7
-# think: 1
-# analyst's,: 1
-# beta: 1
-# testing,: 1
-# hackers: 1
-# feel: 1
-# megabyte: 1
-# one: 2
-# ...
-# you'll: 1
-# so: 2
-# ultimate: 1
-# typing,: 1
-# dos: 1
-# the: 15
-# alone,: 1
-# know: 1
 # Hint: to strip away the periods and parenthesis in the text I recommend doing the following:
 #
 # text = text.replace('.', '').replace('(', '').replace(')', '')
+blues = open('programmers_blues.txt')
+import pickle
+
+blues_string = blues.read()
+
+blues_string = blues_string.replace('.', '').replace("'", '').replace(')', '').replace(',', '').replace('!', '').replace("(", '')
+blues_string = blues_string.lower()
+# print blues_string.split()
+blues_words = blues_string.split()
+
+
+counts = {}
+
+for word in blues_words:
+    current_tally = counts.get(word, 0)
+    new_tally = current_tally + 1
+    counts[word] = new_tally
+
+for word, count in counts.items():
+    print "%d %s's" % (count, word)
+
+# print words
+# for char, count in words.items():
+#     print "%d %s's" % (count, words)
+entries = counts.items()
+version 1
+def mykey((word, count)):
+    return count
+version 2
+entries.sort(key=lambda (word, count): count, reverse=True)
+
+version 3
+entries.sort(key=lambda entry: entry[1], reverse=True)
+
+
+
+for word, count in top_10:
+    print "%s appeared %d times." % (word, count)
+
+version 4
+entries.sort(key=counts.get, reverse=True)
+
+top_10 = entries[0:10]
